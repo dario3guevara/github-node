78893a362bbac34df346d74b2e3296fb77184e37
dario3guevara <dario_guevara@hotmail.com>
2026-02-20 16:03:43 -0500

    Init Project

---
+++ test/sum.test.js
@@ -0,0 +1,11 @@
+import  { describe, it, expect } from "vitest"
+
+import { sum } from "../src/sum"
+
+describe("sum", () => {
+    it("should return the sum of two numbers", () => {
+        expect(sum(2, 3)).toBe(5);
+    })
+})
+
+
